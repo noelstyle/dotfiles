a934dc3fd81f9f5c55ff7698f7bb7e7888c0704c
Choi Jaehoon <plaintext@andromedarabbit.net>
2016-06-16 10:57:52 +0900

    Add `git-standup` and `git-wip`

--- git/install/install.sh
+++ git/install/install.sh
@@ -2,3 +2,16 @@
 THIS_DIR=$(cd "$(dirname "$0")"; pwd)
 
 brew bundle --file="$THIS_DIR/Brewfile"
+
+# Install git-wip
+GIT_WIP_DIR="${HOME}/bin/git-wip"
+
+if [ ! -d ${GIT_WIP_DIR} ]
+then
+  git clone git://github.com/bartman/git-wip.git "${GIT_WIP_DIR}"
+fi
+
+if [ ! -L /usr/local/bin/git-wip ]
+then
+  ln -s "${GIT_WIP_DIR}/git-wip" /usr/local/bin/git-wip
+fi
