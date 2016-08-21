378332e319e9fab4d25f8174c470e95892549c1e
Choi Jaehoon <plaintext@andromedarabbit.net>
2016-08-21 14:12:27 +0900

    Install several tools

--- apps/install/install.sh
+++ apps/install/install.sh
@@ -2,3 +2,6 @@
 THIS_DIR=$(cd "$(dirname "$0")"; pwd)
 
 brew bundle --file="$THIS_DIR/Brewfile"
+
+# ref: https://github.com/yadayada/acd_cli
+# pip3 install --upgrade git+https://github.com/yadayada/acd_cli.git
