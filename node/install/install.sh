339e6a5c294271e7c68c40d79bb247438038fa0a
Jaehoon Choi <plaintext@andromedarabbit.net>
2016-11-21 09:35:15 +0900

    Add some tools

--- node/install/install.sh
+++ node/install/install.sh
@@ -5,3 +5,5 @@ brew bundle --file="$THIS_DIR/Brewfile"
 
 # See https://github.com/jiahaog/nativefier
 npm list -g nativefier && npm update --silent -g nativefier || npm install --silent -g nativefier
+
+npm list -g npm-check && npm update --silent -g npm-check || npm install --silent -g npm-check
