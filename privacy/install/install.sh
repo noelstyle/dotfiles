753e42210538ad6df5a12ba8a559f5e8a5454260
Jaehoon Choi <plaintext@andromedarabbit.net>
2018-10-05 00:32:29 +0900

    dnccrypt-proxy 2.0

--- privacy/install/install.sh
+++ privacy/install/install.sh
@@ -4,7 +4,14 @@ THIS_DIR=$(cd "$(dirname "$0")"; pwd)
 brew bundle --file="$THIS_DIR/Brewfile"
 
 # See https://www.maketecheasier.com/change-dns-servers-terminal-mac/
-sudo networksetup -setdnsservers "Wi-Fi" 8.8.8.8 8.8.4.4
-# sudo networksetup -setdnsservers "Thunderbolt Ethernet" 8.8.8.8 8.8.4.4
+# networksetup -setdnsservers "Wi-Fi" 208.67.222.222 208.67.220.220
+# networksetup -setdnsservers "Thunderbolt Ethernet" 208.67.222.222 208.67.220.220
+
+# networksetup -setdnsservers "Wi-Fi" 8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844
+
+# DNS Crypt
+cp -f "${PWD}/dnscrypt-proxy.toml" "/usr/local/etc/dnscrypt-proxy.toml"
+sudo brew services restart dnscrypt-proxy
+networksetup -setdnsservers "Wi-Fi" 127.0.0.1
 
 keybase pgp pull
